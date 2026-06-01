a9c918ef38d690997eb78e4fed567a8d40769a28
donggcao18 <caovdongg18@gmail.com>
2026-06-01 19:59:12 +0700

    +

--- scripts/yamanishi_compgcn_warm_start.sh
+++ scripts/yamanishi_compgcn_warm_start.sh
@@ -24,7 +24,7 @@ python src/kge_nfm.py \
   --kge-epochs 50 \
   --kge-batch-size 8192 \
   --kge-num-negs 5 \
-  --compgcn-layers 1 \
+  --compgcn-layers 4 \
   --compgcn-dropout 0.1 \
   --compgcn-composition mult \
   --nfm-epochs 200 \
